471a082a88b734f840ca4b63bdc74971ad6fa541
Kenneth Ye <30275095+kennethye1@users.noreply.github.com>
2020-09-09 20:23:36 -0400

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,15 +1,17 @@
-import pygame 
+import pygame, sys
+import random
 
 RED = (255, 0, 0)
 TURQUOISE = (41, 216, 216)
+WHITE = (255, 255, 255)
 pygame.init()
 WIDTH = 1000
 HEIGHT = 800
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 class Bar:
-    def __init__(self, value, width, height, pos, color):
+    def __init__(self, value, width, pos):
         self.value = value
         self.width = width
         self.height = height
@@ -26,4 +28,35 @@ class Bar:
     def reset(self):
         self.color = TURQUOISE
     
-def bubble_sort(list):
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        swap = False
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = arr[j]
+                swap = True
+        if not swap:
+            break
+
+
+def main(screen, n, bar_width):
+    screen.fill(WHITE)
+    arr = [Bar(random.randint(1, HEIGHT), bar_width, (0, i*bar_width)) for i in range(n)]
+
+    done = False
+    while not done:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            
+            if even
+
+        pygame.display.update()
+
+main(SCREEN, 10, WIDTH//10)
+    
+    
